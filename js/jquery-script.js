2caa56e2ee87551454c9d2d4b3d917b7d5bfbd93
MathsGb <108474535+MathsGb@users.noreply.github.com>
2023-03-20 13:59:07 -0300

    Código Finalizado

---
+++ js/jquery-script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    
+    $("#botao-cadastrar").click(function(){
+       
+        $("#form-cadastrar").slideToggle("slow");
+        $("#section-login").slideToggle("slow");
+        $("#botao-cadastrar").hide();
+     });
+});
---
+++ js/script.js
@@ -0,0 +1,31 @@
+function topo() {
+    window.scrollTo(
+      {
+          top:0,
+          left:0,
+          behavior:'smooth'
+      }
+  )
+  }
+  
+  //Validação de Login
+  function login () {
+      var logado = 0;
+      var usuario = document.getElementById("usuario").value;
+      var senha = document.getElementById("senha").value;
+  
+  if(usuario == 'admin' && senha == '123456'){
+  window.location = 'index.html'
+  logado = 1;
+  }
+   
+  if(logado == 0){
+      alert("Dados incorretos, Tente novamente!")
+  }
+  
+  
+  }
+  function cadastro (){
+      alert("Cadastrado com sucesso!");
+      window.location.href = "index.html"
+  }
